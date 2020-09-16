c0d9a6a6e2896db02338707f965d52a7024b4d36
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-09-15 23:03:59 -0400

    Updates

---
+++ precache-manifest.92c9c47d8c119690ce1d5d7daefe4bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c072cc41fad067f82ea7031a7f29c8",
+    "url": "/videoapp/index.html"
+  },
+  {
+    "revision": "5d185b769cf54de71760",
+    "url": "/videoapp/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "1c811c1dadebd3eacd21",
+    "url": "/videoapp/static/css/main.60d495ad.chunk.css"
+  },
+  {
+    "revision": "5d185b769cf54de71760",
+    "url": "/videoapp/static/js/2.fa9aa537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videoapp/static/js/2.fa9aa537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c811c1dadebd3eacd21",
+    "url": "/videoapp/static/js/main.e9c51840.chunk.js"
+  },
+  {
+    "revision": "432a77d4b45fc6fa6bd2",
+    "url": "/videoapp/static/js/runtime-main.f6b0e4b4.js"
+  }
+]);
