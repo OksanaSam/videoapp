c54d83694557218573ae50c6130186f4a840a978
Oksana Sam <oksana.o.samokhvalova@gmail.com>
2020-09-22 23:19:52 -0400

    converted into functional components

--- src/App.js
+++ src/App.js
@@ -1,52 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
-import youtube from './Apis/youtube';
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
-// require('dotenv').config();
+import useVideos from './hooks/useVideos';
 
-class App extends Component {
-  state = { videos: [], selectedVideo: null };
+function App() {
+  const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('flowers');
 
-  componentDidMount() {
-    this.onTermSubmit('buildings');
-  }
+  useEffect(() => {
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
-      params: {
-        q: term,
-      },
-    });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
-  };
+  // const onTermSubmit = async (term) => {
+  //   const response = await youtube.get('/search', {
+  //     params: {
+  //       q: term,
+  //     },
+  //   });
+  //   setVideos(response.data.items);
+  //   setSelectedVideo(response.data.items[0]);
+  // };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-    // console.log(video);
-  };
+  // const onVideoSelect = (video) => {
+  //   setSelectedVideo(video);
+  // };
 
-  render() {
-    return (
-      <>
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five  wide column">
-              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={search} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five  wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,42 +1,39 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import 'emoji-mart/css/emoji-mart.css';
-import { Picker } from 'emoji-mart';
+// import { Picker } from 'emoji-mart';
 
-export default class SearchBar extends Component {
-  state = { term: '' };
+export default function SearchBar(props) {
+  const [term, setTerm] = useState('');
 
-  onInputChange = (e) => {
-    this.setState({ term: e.target.value });
-  };
-
-  onFormSubmit = (e) => {
+  const onFormSubmit = (e) => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+
+    props.onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <p>search</p>
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label htmlFor="">
-              <input
-                onChange={this.onInputChange}
-                value={this.state.term}
-                type="text"
-                name=""
-                id=""
-              />
-            </label>
-          </div>
-        </form>
-        {/* <Picker
+  return (
+    <div className="search-bar ui segment">
+      <p>search</p>
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label htmlFor="videoSearch">
+            <input
+              onChange={(e) => {
+                setTerm(e.target.value);
+              }}
+              value={term}
+              type="text"
+              name=""
+              id="videoSearch"
+            />
+          </label>
+        </div>
+      </form>
+      {/* <Picker
           title="Pick your emoji…"
           emoji="point_up"
           onSelect={this.addEmoji}
         /> */}
-      </div>
-    );
-  }
+    </div>
+  );
 }
